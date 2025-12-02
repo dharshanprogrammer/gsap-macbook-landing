fcf8c0c0202981c869d5a590af8d721721d68597
dharshanprogrammer <dharshanprogrammer@gmail.com>
2025-12-03 01:00:36 +0530

    Added Hero Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import NavBar from "./components/Navbar";
+import Hero from "./components/Hero"
 const App = () => {
   return (
     <main>
       <NavBar />
+      <Hero />
     </main>
   );
 };
