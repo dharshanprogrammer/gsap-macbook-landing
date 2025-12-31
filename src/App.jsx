72ea4d02f9d55703d38bb095d9f289c212eafbc1
dharshanprogrammer <dharshanprogrammer@gmail.com>
2026-01-01 00:56:56 +0530

    Implemented Showcase Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import React from "react";
-import NavBar from "./components/Navbar";
+import NavBar from "./components/NavBar";
 import Hero from "./components/Hero"
 import ProductViewer from "./components/ProductViewer";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/all";
 gsap.registerPlugin(ScrollTrigger);
+import Showcase from "./components/ShowcaseCard";
 const App = () => {
   return (
     <main>
       <NavBar />
       <Hero />
       <ProductViewer />
+      <Showcase />
     </main>
   );
 };
